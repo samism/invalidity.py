bb62581eca6da67501fd9aac12cb45cb4874e021
Sameer Ismail <admin@samism.net>
2013-12-18 20:03:48 -0600

    Removed readme info from invalidity.py to the README file itself.

--- invalidity.py
+++ invalidity.py
@@ -1,30 +1,3 @@
-''' 
-This program tests for the invalidity of an argument using 
-an algorithm centered around the shortened truth table method in predicate logic.
-
-The algorithm's objective is to find an instance in which all the premises of an 
-argument can be found true while having the conclusion false in a universe of one member.
-
-If the algorithm cannot find this case in a universe of one member, it will append members
-to the universe until it reaches a universe in which the amount of members allows for this case
-to occur. Many arguments will never arrive at this case because they are likely valid.
-
-If this instance is found, the program will terminate and report how many members a universe
-needs to contain for the invalid case to occur as well as which premises must be true and which false.
-
-It will terminate at the first finding of such a case because if an argument is invalid in one universe,
-it is invalid in all universes. 
-
-Due to computional limitations of most computers, the upper-bound for the amount of members
-appendable to the universe is 100.
-
-Universally quantified statement 	- (x) -> implies horshoe
-Existentially quantified statement 	- (Ex) -> implies dot
-
-Predicates are capital letters A-Z
-Constants are small letters a-w
-'''
-
 import sys
 import os
 
