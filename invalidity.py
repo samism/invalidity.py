67b5b968bc14cb1f2f5398a67ef5efcb46f0f43d
Sameer Ismail <admin@samism.net>
2013-12-20 23:33:06 -0600

    Changed some little things.

--- invalidity.py
+++ invalidity.py
@@ -2,21 +2,23 @@ import sys
 import os
 
 class Invalidity(object):
+	# computes which variables must be T or F to arrive at an invalid case (1 universe, first)
+	def invalidCase(self, statements):
+		return 'foo-bar'
 
 	# instantiates quantified statement for a given member of the universe
-
 	def instantiateStatement(self, statement, member):
 		i_statement = ''
 		pred = statement[statement.find(')') + 1:len(statement)].split('x') # list of predicates
 		pred = pred[:-1] # don't need first arg (filename)
-		print 'Predicates from statement: ' + str(pred)
+		#print 'Predicates from statement: ' + str(pred)
 
 		for i in pred:
-			i_statement = i_statement + i + member
+			i_statement += i + member
 			if statement.startswith('(x)') and pred[0] == i:
-				i_statement = i_statement + '.'
+				i_statement += '.'
 			elif statement.startswith('(Ex)') and pred[0] == i:
-				i_statement = i_statement + 'v'
+				i_statement += 'v'
 
 		return i_statement
 
@@ -31,11 +33,12 @@ class Invalidity(object):
 			print 'where premise/conc formatted: ((E)x)A-ZxA-Zx'
 			exit(1)
 
+		# obtain instantiated forms of the given statements
 		for i in args:
 			for j in self.members:
 				self.statements.append(self.instantiateStatement(i, j))
 
-		print str(self.statements)
-
+		#...
+		print self.invalidCase(self.statements)
 
 Invalidity(sys.argv)
