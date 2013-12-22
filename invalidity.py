ba3bd7557effe3d88112f2b35c482ce3f3ad799d
Sameer Ismail <admin@samism.net>
2013-12-22 08:56:45 -0600

    Interesting changes. Decoupling some functions and other changes.

--- invalidity.py
+++ invalidity.py
@@ -6,17 +6,18 @@ class Invalidity(object):
 	def invalidCase(self, statements):
 		pass
 
-	# parses out individual predicates
+	# parses out individual predicates in their instantiated forms
 	# transfer parsing stuff from instantiateStatement() to here and leave actual instantiation
-	def obtainVariables(self, args):
-		pass
+	def obtainPredicates(self, statement, member):
+		preds = statement[statement.find(')') + 1:len(statement)].split('x') # list of predicates
+		for i in range(len(preds)):
+			for j in members
+			preds[i] += j
+		return preds[:-1]
 
 	# instantiates quantified statement for a given member of the universe
-	def instantiateStatement(self, statement, member):
+	def instantiateStatement(self, predicate, member):
 		i_statement = ''
-		pred = statement[statement.find(')') + 1:len(statement)].split('x') # list of predicates
-		pred = pred[:-1]
-		#print 'Predicates from statement: ' + str(pred)
 
 		for i in pred:
 			i_statement += i + member
@@ -32,7 +33,7 @@ class Invalidity(object):
 
 		self.members = ['a'] # universe begins with a single member - a
 		self.statements = list() # format for a valid premise/conclusion -> ((E)x)A-ZxA-Zx
-		self.variables = list() # individual predicates
+		self.predicates = list() # individual predicates
 
 		if len(args) < 1 or len(args) > 3:
 			print 'Usage: python invalidity.py "[premise]" "[premise]" "[conclusion]"'
@@ -42,6 +43,7 @@ class Invalidity(object):
 		# obtain instantiated forms of the given statements
 		for i in args:
 			for j in self.members:
+				self.predicates.append(self.obtainPredicates(i, j))
 				self.statements.append(self.instantiateStatement(i, j))
 
 		print self.statements
